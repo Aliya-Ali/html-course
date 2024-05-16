df84f08667e13232f6ce65890b88f4254bc2e70a
Aliya Ali <aliyaali33804@gmail.com>
2024-05-16 23:00:22 +0500

    first message

--- week 2/day4/script.js
+++ week 2/day4/script.js
@@ -9,3 +9,56 @@ let num2 = 5
 if (num == 5 || num2 == 5){
     console.log("either num or num2 is equal to 5")
 }
+if (!(num== num2)){
+    console.log("nums and num2 values are equal")
+}
+
+// day = "Monday"
+// if (day =="Monday"){
+//     console.log("right day")
+// }
+// else if (day == "Tuesday"){
+//     console.log("wrong day")
+// }
+// else if (day == "Wednesday"){
+//     console.log("wrong day")
+// }
+// else if (day == "Thursday"){
+//     console.log("wrong day")
+// }
+// else if (day == "Friday"){
+//     console.log("wrong day")
+// }
+// else if (day == "Saturday"){
+//     console.log("wrong day")
+// }
+// else if (day == "Sunday"){
+//     console.log("wrong day")
+// }
+day = "Monday"
+switch(day){
+    case "Monday":
+        console.log("correct day")
+        break;
+    case "Tuesday":
+        console.log("wrong day")
+        break;
+    case "Wednesday":
+        console.log("wrong day")
+        break;
+    case "Thursday":
+        console.log("wrong day")
+        break;
+    case "Friday":
+        console.log("wrong day")
+        break;
+    case "Saturday":
+        console.log("wrong day")
+        break;
+    case "Sunday":
+        console.log("wrong day")
+        break;
+}
+for (let i =0; i<10; i++){
+    console.log("2 X "+ i + " = " + 2*i )
+}
