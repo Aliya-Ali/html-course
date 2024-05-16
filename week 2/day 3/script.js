1d773ecb0f452c3e5ff8b40a2bb04e8e59bb64ee
Aliya Ali <aliyaali33804@gmail.com>
2024-05-16 22:26:25 +0500

    first message

---
+++ week 2/day 3/script.js
@@ -0,0 +1,32 @@
+console.log("Hello World")
+// console.log("This is the first javascript code")
+// let num = 20;
+// var num2 = 30;
+// const num3 = 40;//integer
+// let num4 = 20.0001; // float
+// let message = "Hello this is my first message" //string
+// let message2 = true; //boolean
+
+// // this is the introduction of data types
+// console.log(num)
+// console.log(num4)
+// console.log(message)
+// console.log(message2)
+
+// console.log(2 +2) // addition
+// console.log("2 +2") // string
+// console.log(2 - 2) //subtraction
+// console.log(2 * 2) // multiplication
+// console.log(2 / 2) // division
+
+// let num = 2
+// let num2 = 3
+// console.log(num + num2)
+// let sum = num + num2
+// console.log(sum)
+
+//Assignment Operators
+let num = 2;
+num = num + 5;
+num += 5
+console.log(num)
---
+++ week 2/day4/script.js
@@ -0,0 +1,11 @@
+// let num =2 
+// if (num<10){
+//     console.log("nums is less than 10")
+// }else{
+//     console.log("num is greater than 10")
+// }
+let num =5
+let num2 = 5
+if (num == 5 || num2 == 5){
+    console.log("either num or num2 is equal to 5")
+}
